d3e9421a1508a01cbac7b3dbf7e4d7cd74a60e6c
rubgonza <tsystemsrubgonza@gmail.com>
2022-05-26 16:47:22 +0200

    Passport

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -13,6 +13,9 @@ var leaderRouter = require("./routes/leaderRouter");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
 
+var passport = require("passport");
+var authenticate = require("./authenticate");
+
 const mongoose = require("mongoose");
 
 const url = "mongodb://localhost:27017/conFusion";
@@ -31,6 +34,9 @@ var app = express();
 
 app.use(cookieParser("12345-67890-09876-54321"));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(
   session({
     name: "session-id",
@@ -45,20 +51,14 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
     err.status = 403;
-    return next(err);
+    next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
 
